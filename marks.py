511b470f204e3c3eb1786944e6b572b18d721055
JavganJG <Javgan00@gmail.com>
2021-09-29 08:33:49 +0100

    First version

---
+++ marks.py
@@ -0,0 +1,11 @@
+n = float(input("Mark: "))
+if n < 5:
+    print("Fail!")
+elif n < 6:
+    print("Enough")
+elif n < 7:
+    print("Good!")
+elif n < 9:
+    print("Very good!!")
+else:
+    print("Excellent!!!")
